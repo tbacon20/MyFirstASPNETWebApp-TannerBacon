fc61608c547d1f24d2b01f669889a9503a73338b
Tanner Bacon <tannerbacon20@gmail.com>
2023-01-28 18:47:08 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp-TannerBacon/wwwroot/js/FanMail.js
@@ -0,0 +1,15 @@
+﻿
+/*
+document.getElementById("btnSend").addEventListener("click", function () {
+    alert("From: " + document.getElementById("txtFrom").value +
+        ", Subject: " + document.getElementById("txtSubject") +
+        ", Message: " + document.getElementById("txtMessage"));
+})
+*/
+
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val()
+    );
+})
