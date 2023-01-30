1793da014a3c4b3c2a2e60214f921613465ec137
Tanner Bacon <tannerbacon20@gmail.com>
2023-01-30 09:26:28 -0700

    Adding grade form

---
+++ MyFirstASPNETWebApp-TannerBacon/wwwroot/js/Grades.js
@@ -0,0 +1,14 @@
+﻿$("#btnSend").click(function () {
+
+    var finalGrade;
+    
+    finalGrade = ($("#assignments").val() * 0.5) +
+        ($("#groupproject").val() * 0.1) +
+        ($("#quizzes").val() * 0.1) +
+        ($("#midterm").val() * 0.1) +
+        ($("#final").val() * 0.1) + 
+        ($("#intex").val() * 0.1);
+
+    $("#gradeform").append("Final grade: " + finalGrade.toString());
+    //alert("Final grade: " + $("#assignments").val());
+})
