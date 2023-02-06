40dcf77d749856f824dbc2bde961f238937598f4
Tanner Bacon <tannerbacon20@gmail.com>
2023-02-06 09:35:06 -0700

    Adding MVC

---
+++ MyFirstASPNETWebApp-TannerBacon/Controllers/RazorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNETWebApp_TannerBacon.Controllers
+{
+    public class RazorController : Controller
+    {
+        public IActionResult Index()
+        {
+            // This will purposely throw an error
+            // throw new IndexOutOfRangeException();
+            
+            // will end up doing something like return View(GradeForm);
+            return View();
+        }
+    }
+}
--- MyFirstASPNETWebApp-TannerBacon/Startup.cs
+++ MyFirstASPNETWebApp-TannerBacon/Startup.cs
@@ -16,16 +16,38 @@ namespace MyFirstASPNETWebApp_TannerBacon
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Set up the services (controllers with views)
+            services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            // Configure required to start app in MVC mode
+            app.UseStaticFiles();
+            app.UseRouting();
+            // Assigns endpoint to the correct controller and action with an optional (?) id
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Razor}/{action=Index}/{id?}"
+                    );
+            });
+            
+            /* This is the configuration used without MVC
             // This will allow default files to load first (index.html is a default)
             app.UseDefaultFiles();
 
             // This allows the app to use our static page files (in wwwroot)
             app.UseStaticFiles();
+            */
+
 
             /* This is the default configure method
             if (env.IsDevelopment())
